d3e1d0fac8246cee222934d8bb77da49fa8be28f
Maciej Dems <maciej.dems@p.lodz.pl>
2024-02-11 17:32:13 +0100

    Add load and save

--- simple_plot/mainwindow.py
+++ simple_plot/mainwindow.py
@@ -10,6 +10,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import csv
+import re
+
 import numpy as np
 from matplotlib.backends.backend_qtagg import FigureCanvas
 from matplotlib.backends.backend_qtagg import NavigationToolbar2QT as NavigationToolbar
@@ -23,6 +26,9 @@ from scipy.stats import linregress
 from . import simpleplot_rc  # noqa
 from .mainwindow_ui import Ui_MainWindow
 from .util import BlockQtSignals
+
+HEADER_RE = re.compile(r"^(?P<name>.+?)\s*(?:\((?P<symbol>.+?)\))?\s*(?:\[(?P<unit>.+?)\])?$")
+SYMBOL_RE = re.compile(r"[a-zA-Z_]\w*")
 
 
 class DoubleValidator(QDoubleValidator):
@@ -46,25 +52,31 @@ class MainWindow(Ui_MainWindow, QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.filename = ""
+
         self.setupUi(self)
-        self.connectSignals()
 
         self.retranslateUi(self)
+
+        symbol_regex = QRegExp(r"[a-zA-Z_]\w*")
+        self.xsymbol.setValidator(QRegExpValidator(symbol_regex, self))
+        self.ysymbol.setValidator(QRegExpValidator(symbol_regex, self))
 
         self.data_table.setItemDelegate(FloatDelegate(self))
         dataitem = QTableWidgetItem()
         dataitem.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
         self.data_table.setItemPrototype(dataitem)
 
-        symbol_regex = QRegExp(r'[a-zA-Z_]\w*')
-        self.xsymbol.setValidator(QRegExpValidator(symbol_regex, self))
-        self.ysymbol.setValidator(QRegExpValidator(symbol_regex, self))
+        self.insert_row_action = QAction(self.tr("Insert Row"), self)
+        self.data_context_menu = QMenu(self)
+        self.data_context_menu.addAction(self.insert_row_action)
+        self.data_table.setContextMenuPolicy(Qt.CustomContextMenu)
 
         self.add_plot()
-
+        self.connect_signals()
         self.process_data()
 
-    def connectSignals(self):
+    def connect_signals(self):
         self.xsymbol.textChanged.connect(self.on_xsymbol_changed)
         self.ysymbol.textChanged.connect(self.on_ysymbol_changed)
         self.xunit.textChanged.connect(self.on_unit_or_name_changed)
@@ -72,12 +84,29 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         self.xname.textChanged.connect(self.on_unit_or_name_changed)
         self.yname.textChanged.connect(self.on_unit_or_name_changed)
         self.data_table.itemChanged.connect(self.on_data_changed)
+        self.load_action.triggered.connect(self.load)
+        self.save_action.triggered.connect(self.save)
+        self.export_action.triggered.connect(self.figure_toolbar.save_figure)
+        self.data_table.customContextMenuRequested.connect(self.on_data_context_menu)
 
     def add_plot(self):
         self.figure = Figure(figsize=(5, 3))
         static_canvas = FigureCanvas(self.figure)
-        self.plot_layout.addWidget(NavigationToolbar(static_canvas, self))
+        self.figure_toolbar = NavigationToolbar(static_canvas, self)
+        self.plot_layout.addWidget(self.figure_toolbar)
         self.plot_layout.addWidget(static_canvas)
+
+    def on_data_context_menu(self, pos):
+        index = self.data_table.indexAt(pos)
+        if not index.isValid(): return
+        action = self.data_context_menu.exec_(self.data_table.viewport().mapToGlobal(pos))
+
+        if action == self.insert_row_action:
+            row = index.row()
+            self.data_table.insertRow(row)
+            index = self.data_table.model().index(row, 0)
+            self.data_table.setCurrentIndex(index)
+            self.data_table.edit(index)
 
     def on_xsymbol_changed(self, text):
         self.data_table.setHorizontalHeaderItem(0, QTableWidgetItem(self.xsymbol.text()))
@@ -106,10 +135,10 @@ class MainWindow(Ui_MainWindow, QMainWindow):
 
     def get_data(self):
         rows = self.data_table.rowCount() - 1
-        x = np.fromiter((x and x.text() and float(x.text()) or np.nan for x in (self.data_table.item(i, 0) for i in range(rows))),
-                        dtype=float)
-        y = np.fromiter((y and y.text() and float(y.text()) or np.nan for y in (self.data_table.item(i, 1) for i in range(rows))),
-                        dtype=float)
+        x = (x and x.text() or 'nan' for x in (self.data_table.item(i, 0) for i in range(rows)))
+        x = np.fromiter((float(x) for x in x), dtype=float)
+        y = (y and y.text() or 'nan' for y in (self.data_table.item(i, 1) for i in range(rows)))
+        y = np.fromiter((float(y) for y in y), dtype=float)
 
         return x, y
 
@@ -119,8 +148,8 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         x, y = self.get_data()
 
         if len(x) < 2:
-            self.slope.setText('')
-            self.intercept.setText('')
+            self.slope.setText("")
+            self.intercept.setText("")
             return
 
         result = linregress(x, y)
@@ -128,19 +157,89 @@ class MainWindow(Ui_MainWindow, QMainWindow):
 
         xu = self.xunit.text()
         yu = self.yunit.text()
-        unit = f" {yu} / {xu}" if xu and yu else f" {yu}" if yu else f" / {xu}" if xu else ""
+        yu = f" {yu}" if yu else ""
+        unit = f"{yu} / {xu}" if xu and yu else yu if yu else f" / {xu}" if xu else ""
         self.slope.setText(f"({a:.3f} ± {da:.3f}){unit}")
-        self.intercept.setText(f"({b:.3f} ± {db:.3f}){f' {yu}' if yu else ''}")
+        self.intercept.setText(f"({b:.3f} ± {db:.3f}){yu}")
 
         ax = self.figure.add_subplot(111)
-        ax.plot(x, y, 'o')
+        ax.plot(x, y, "o")
 
         xx = np.array(ax.get_xlim())
         ax.plot(xx, a * xx + b)
         ax.set_xlim(xx)
 
-        ax.set_xlabel(f"{self.xname.text()} {f'[{xu}]' if xu else ''}")
-        ax.set_ylabel(f"{self.yname.text()} {f'[{yu}]' if yu else ''}")
+        ax.set_xlabel(f"{self.xname.text() or f'${self.xsymbol.text()}$'} {f'[{xu}]' if xu else ''}")
+        ax.set_ylabel(f"{self.yname.text() or f'${self.ysymbol.text()}$'} {f'[{yu.strip()}]' if yu else ''}")
 
         self.figure.tight_layout()
         self.figure.canvas.draw()
+
+    def load(self):
+        self.filename, _ = QFileDialog.getOpenFileName(
+            self,
+            self.tr("Open File"),
+            self.filename,
+            self.tr("CSV Files (*.csv)"),
+        )
+        if not self.filename:
+            return
+
+        with open(self.filename, "r", newline="") as file:
+            reader = csv.reader(file)
+            header = next(reader)
+            xheader, yheader = header
+            xmatch = HEADER_RE.match(xheader)
+            ymatch = HEADER_RE.match(yheader)
+            if xmatch:
+                self.xname.setText(xmatch.group("name") if xmatch.group("symbol") else "")
+                self.xsymbol.setText(xmatch.group("symbol") or xmatch.group("name"))
+                self.xunit.setText(xmatch.group("unit") or "")
+            if ymatch:
+                self.yname.setText(ymatch.group("name") if ymatch.group("symbol") else "")
+                self.ysymbol.setText(ymatch.group("symbol") or ymatch.group("name"))
+                self.yunit.setText(ymatch.group("unit") or "")
+
+            data = list(reader)
+            ld = len(data)
+            self.data_table.setRowCount(ld + 1)
+            with BlockQtSignals(self.data_table):
+                for i, (xi, yi) in enumerate(data):
+                    xitem = QTableWidgetItem(xi)
+                    yitem = QTableWidgetItem(yi)
+                    xitem.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+                    yitem.setTextAlignment(Qt.AlignRight | Qt.AlignVCenter)
+                    self.data_table.setItem(i, 0, xitem)
+                    self.data_table.setItem(i, 1, yitem)
+                self.data_table.setItem(ld, 0, QTableWidgetItem())
+                self.data_table.setItem(ld, 1, QTableWidgetItem())
+
+        self.process_data()
+
+    @staticmethod
+    def _make_header(name, symbol, unit):
+        if not name:
+            return f"{symbol} [{unit}]" if unit else f"{symbol}" if symbol else ""
+        else:
+            return f"{name} ({symbol}) [{unit}]" if unit else f"{name} ({symbol})" if symbol else name
+
+    def save(self):
+        self.filename, _ = QFileDialog.getSaveFileName(
+            self,
+            self.tr("Save File"),
+            self.filename,
+            self.tr("CSV Files (*.csv)"),
+        )
+        if not self.filename:
+            return
+
+        with open(self.filename, "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([
+                self._make_header(self.xname.text(), self.xsymbol.text(), self.xunit.text()),
+                self._make_header(self.yname.text(), self.ysymbol.text(), self.yunit.text())
+            ])
+
+            x, y = self.get_data()
+            for xi, yi in zip(x, y):
+                writer.writerow([xi, yi])
--- simple_plot/translations/simpleplot_pl.ts
+++ simple_plot/translations/simpleplot_pl.ts
@@ -9,57 +9,89 @@
         <translation>Prosty wykres</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="23"/>
-        <location filename="mainwindow.ui" line="76"/>
-        <source>Symbol</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="33"/>
+        <location filename="mainwindow.ui" line="37"/>
         <source>Vertical Axis</source>
         <translation>Oś pionowa</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="40"/>
-        <location filename="mainwindow.ui" line="60"/>
+        <location filename="mainwindow.ui" line="64"/>
         <source>Unit</source>
         <translation>Jednostka</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="50"/>
-        <source>x</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="93"/>
-        <source>y</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="100"/>
+        <location filename="mainwindow.ui" line="104"/>
         <source>Horizontal Axis</source>
         <translation>Oś pozioma</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="107"/>
-        <location filename="mainwindow.ui" line="120"/>
+        <location filename="mainwindow.ui" line="124"/>
         <source>Name</source>
         <translation>Nazwa</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="130"/>
+        <location filename="mainwindow.ui" line="134"/>
         <source>Fit Results</source>
         <translation>Wyniki dopasowania</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="157"/>
+        <location filename="mainwindow.ui" line="161"/>
         <source>Slope:</source>
         <translation>Nachylenie:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="164"/>
+        <location filename="mainwindow.ui" line="168"/>
         <source>Intercept:</source>
         <translation>Przecięcie:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="207"/>
+        <source>&amp;Load</source>
+        <translation>&amp;Wczytaj</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="215"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Zapisz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="80"/>
+        <source>Symbol</source>
+        <translation>Symbol</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <source>x</source>
+        <translation>x</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="97"/>
+        <source>y</source>
+        <translation>y</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="180"/>
+        <source>Open File</source>
+        <translation>Otwórz plik</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="230"/>
+        <source>CSV Files (*.csv)</source>
+        <translation>Plik CSV (*.csv)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="228"/>
+        <source>Save File</source>
+        <translation>Zapisz do pliku</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.py" line="59"/>
+        <source>Insert Row</source>
+        <translation>Wstaw wiersz</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="223"/>
+        <source>Save &amp;figure</source>
+        <translation>Zapisz &amp;obraz</translation>
     </message>
 </context>
 </TS>
